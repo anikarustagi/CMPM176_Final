0ae025c3994198d14e045d2413cd0e7547eed7c4
aneeshasharma <ashar160@ucsc.edu>
2026-03-17 18:46:18 -0700

    added start screen and instructions will polish later

--- camera_2d.gd
+++
@@ -1,6 +0,0 @@
-extends Camera2D
-
-@onready var player = get_node("../Player") # change "Player" to whatever your player node is named
-
-func _process(delta):
-	global_position = lerp(global_position, player.global_position, 0.1)
